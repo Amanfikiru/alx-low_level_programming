a1c18d48af53848aab4920970a93c29a2ca7fc4b
amanfikiru <Amanuelfikiru49@gmail.com>
2022-04-08 05:42:16 -0700

    print alphabet in lowercase and uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,13 @@ int main(void)
 {
 	char c = 'a';
 	char C = 'A';
-	while(c<='z')
+
+	while(c <= 'z')
 	{
 		putchar(c);
 		c++;
 	}
-	while(C<='Z')
+	while(C <= 'Z')
         {
                 putchar(C);
                 C++;
