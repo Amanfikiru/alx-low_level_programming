d2703b29125a128daaf4350fd9eae212096cb9ba
amanfikiru <Amanuelfikiru49@gmail.com>
2022-04-08 06:17:50 -0700

    print numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet except the letters q and e.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int n = 0;
+	while (n >= 10)
+	{
+		printf("%d\n", n);
+	}
+	return (0);
+}
