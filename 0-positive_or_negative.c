b4f1658d8cb992da1738f99c6e391f57457276b9
amanfikiru <Amanuelfikiru49@gmail.com>
2022-04-08 04:24:57 -0700

    generate random number

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+	int num = rand();
+	if (num < 0)
+		printf("%d is negative\n", num);
+	else if (num > 0)
+		printf("%D is positive\n", num);
+	else 
+		printf("%d is zero\n", num);
+	return (0);
+}
