41895e96259728a925822888646a9197d1159a61
amanfikiru <Amanuelfikiru49@gmail.com>
2022-04-08 06:52:12 -0700

    prints all the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -  prints the lowercase alphabet in reverse, followed by a new line.
+ * and putchar 
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char n = 'z';
+
+	while (n >= 'a')
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
