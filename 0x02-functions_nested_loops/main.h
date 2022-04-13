18b677abbd27d17cf0035081ee9a8ead45a18215
amanfikiru <Amanuelfikiru49@gmail.com>
2022-04-12 20:37:56 -0700

    succesfully run

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#define _MAIN_H_
+#ifndef _MAIN_H_
 #define _MAIN_H_
 
 int _putchar(char c);
@@ -14,4 +14,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 
-#endif /* _MAIN_H_ */
+#endif 
