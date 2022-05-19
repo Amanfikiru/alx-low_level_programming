cc86fcbfc6c2c7d5cc13730364484f89cbffb5ed
amanfikiru <Amanuelfikiru49@gmail.com>
2022-05-19 11:04:31 -0700

    print all element of list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -8,8 +10,8 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
+
 typedef struct list_s
 {
 	char *str;
