25c5fa32f1e9847070049d2efb50500af971fd35
amanfikiru <Amanuelfikiru49@gmail.com>
2022-07-11 11:23:18 -0700

    dynamic lib

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/alx-low_level_programming/raw/master/0x17-dynamic_libraries/libmask.so
+wget -P /tmp https://github.com/Amanfikiru/alx-low_level_programming/raw/master/0x17-dynamic_libraries/libmask.so
 export LD_PRELOAD=/tmp/libmask.so
